17c96e1af3e818f4b3e45741dc3723b1b74607e8
Makar <makardrozdik00@gmail.com>
2022-11-23 21:48:08 +0200

    Добавьте файлы проекта.

---
+++ Individual Assignment 11/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Individual_Assignment_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.typeTextbox = new System.Windows.Forms.TextBox();
+            this.ageTextbox = new System.Windows.Forms.TextBox();
+            this.panelScreen = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(441, 29);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(79, 28);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(100, 22);
+            this.nameTextbox.TabIndex = 1;
+            // 
+            // typeTextbox
+            // 
+            this.typeTextbox.Location = new System.Drawing.Point(194, 29);
+            this.typeTextbox.Name = "typeTextbox";
+            this.typeTextbox.Size = new System.Drawing.Size(100, 22);
+            this.typeTextbox.TabIndex = 2;
+            // 
+            // ageTextbox
+            // 
+            this.ageTextbox.Location = new System.Drawing.Point(318, 29);
+            this.ageTextbox.Name = "ageTextbox";
+            this.ageTextbox.Size = new System.Drawing.Size(100, 22);
+            this.ageTextbox.TabIndex = 3;
+            // 
+            // panelScreen
+            // 
+            this.panelScreen.Location = new System.Drawing.Point(0, 56);
+            this.panelScreen.Name = "panelScreen";
+            this.panelScreen.Size = new System.Drawing.Size(788, 312);
+            this.panelScreen.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelScreen);
+            this.Controls.Add(this.ageTextbox);
+            this.Controls.Add(this.typeTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.addBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.TextBox typeTextbox;
+        private System.Windows.Forms.TextBox ageTextbox;
+        private System.Windows.Forms.Panel panelScreen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Individual Assignment 11/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
+namespace Individual_Assignment_11
+{
+    public partial class Form1 : Form
+    {
+        Pet[] pets = new Pet[10];
+        int petsCreated=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if (petsCreated != 10)
+            {
+                try
+                {
+                    pets[petsCreated] = new Pet(nameTextbox.Text, typeTextbox.Text, Int32.Parse(ageTextbox.Text));
+                    petsCreated++;
+                    refreshPanel();
+                } catch
+                {
+                    MessageBox.Show("Wrong input");
+                }
+            }
+        }
+
+        public void refreshPanel()
+        {
+            panelScreen.Controls.Clear();
+            if (pets[0] != null)
+            {
+                Label newLabel1 = new Label
+                {
+                    Text = pets[0].getName + " " + pets[0].getType + " " + pets[0].getAge
+                };
+                panelScreen.Controls.Add(newLabel1);
+
+                newLabel1.Location = new Point(1, 1);
+            }
+            if (pets[1] != null)
+            {
+                Label newLabel2 = new Label
+                {
+                    Text = pets[1].getName + " " + pets[1].getType + " " + pets[1].getAge
+                };
+                panelScreen.Controls.Add(newLabel2);
+                newLabel2.Location = new Point(1, 20);
+            }
+            if (pets[2] != null)
+            {
+                Label newLabel3 = new Label
+                {
+                    Text = pets[2].getName + " " + pets[2].getType + " " + pets[2].getAge
+                };
+                panelScreen.Controls.Add(newLabel3);
+                newLabel3.Location = new Point(1, 40);
+            }
+            if (pets[3] != null)
+            {
+                Label newLabel4 = new Label
+                {
+                    Text = pets[3].getName + " " + pets[3].getType + " " + pets[3].getAge
+                };
+                panelScreen.Controls.Add(newLabel4);
+                newLabel4.Location = new Point(1, 60);
+            }
+            if (pets[4] != null)
+            {
+                Label newLabel5 = new Label
+                {
+                    Text = pets[4].getName + " " + pets[4].getType + " " + pets[4].getAge
+                };
+                panelScreen.Controls.Add(newLabel5);
+                newLabel5.Location = new Point(1, 80);
+            }
+            if (pets[5] != null)
+            {
+                Label newLabel5 = new Label
+                {
+                    Text = pets[5].getName + " " + pets[5].getType + " " + pets[5].getAge
+                };
+                panelScreen.Controls.Add(newLabel5);
+                newLabel5.Location = new Point(1, 100);
+            }
+            if (pets[6] != null)
+            {
+                Label newLabel5 = new Label
+                {
+                    Text = pets[6].getName + " " + pets[6].getType + " " + pets[6].getAge
+                };
+                panelScreen.Controls.Add(newLabel5);
+                newLabel5.Location = new Point(1, 120);
+            }
+            if (pets[7] != null)
+            {
+                Label newLabel6 = new Label
+                {
+                    Text = pets[7].getName + " " + pets[7].getType + " " + pets[7].getAge
+                };
+                panelScreen.Controls.Add(newLabel6);
+                newLabel6.Location = new Point(1, 140);
+            }
+            if (pets[8] != null)
+            {
+                Label newLabel7 = new Label
+                {
+                    Text = pets[8].getName + " " + pets[8].getType + " " + pets[8].getAge
+                };
+                panelScreen.Controls.Add(newLabel7);
+                newLabel7.Location = new Point(1, 160);
+            }
+            if (pets[9] != null)
+            {
+                Label newLabel8 = new Label
+                {
+                    Text = pets[9].getName + " " + pets[9].getType + " " + pets[9].getAge
+                };
+                panelScreen.Controls.Add(newLabel8);
+                newLabel8.Location = new Point(1, 180);
+            }
+
+        }
+
+    }
+    public class Pet { 
+        private string petName;
+        private string petType;
+        private int petAge;
+        public Pet(string name, string type, int age)
+        {
+            petName = name;
+            petType = type;
+            petAge = age;
+        }
+        public string getName
+        {
+            get { return petName; }
+        }
+        public string getType { 
+            get { return petType; }
+        }
+        public int getAge
+        {
+            get { return petAge; }
+        }
+    }
+}
